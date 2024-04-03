878d7f0069129380400d3d4e0e067e6eebecbdee
Nicolas Jonckheere <nicolasjonckheere22@hotmail.com>
2024-04-03 15:19:19 +0000

    updated waiter table

--- restaurant_dbt/models/restaurant_tables_silver/tb_waiter.sql
+++ restaurant_dbt/models/restaurant_tables_silver/tb_waiter.sql
@@ -6,32 +6,32 @@
     Try changing "table" to "view" below
 */
 
+
 SELECT
-  id_waiter,
-  SUM(IF(
-    payed_price - order_price > 0,
-    payed_price - order_price ,
-    0
-  )) AS sum_tip,
+  od.id_order_line as item_id,
+  od.dim_name_translated as item_name,
+  od.dim_category_translated as item_category,
+  od.m_unit_price as item_price,
+  od.m_quantity as item_quantity,
+  o.id_order,
+  o.id_store,
+  o.id_table,
+  o.id_waiter,
+  CAST(o.date_opened AS TIMESTAMP) AS order_date_opened,
+  CAST(o.date_closed AS TIMESTAMP) AS order_date_closed,
+  o.dim_status,
+  CAST(o.m_cached_price AS NUMERIC) AS order_price,
+  CAST(o.m_cached_payed AS NUMERIC) AS payed_price,
+  CAST(o.m_nb_customer AS INTEGER) AS number_customer
+ 
+FROM {{ source ('restaurant_raw_data','orders')}} AS o
+INNER JOIN {{ source ('restaurant_raw_data','order_details')}} AS od
+USING (id_order)
+WHERE o.id_order IS NOT NULL
+AND o.id_store IS NOT NULL
+AND od.m_unit_price > 0
 
-  SUM(IF(
-    payed_price - order_price < 0,
-    payed_price - order_price ,
-    0
-  )) AS sum_unpayed_order,
-
-  COUNT(DISTINCT CONCAT(id_table, id_order)) AS nb_tables,
-
-  AVG(TIMESTAMP_DIFF(order_date_opened , order_date_closed , hour)) AS avg_working_hours,
-
-  COUNT(DISTINCT CASE WHEN dim_status LIKE 'CLOSED' THEN id_order END)AS closed_orders,
-  COUNT(DISTINCT CASE WHEN dim_status NOT LIKE 'CLOSED' THEN id_order END)AS un_closed_orders,
-
-
-FROM {{ ref ('tb_sales') }}
-WHERE id_waiter IS NOT NULL
-GROUP BY id_waiter
-
+QUALIFY ROW_NUMBER() OVER(PARTITION BY  id_order, id_store ORDER BY  order_date_closed DESC ) = 1
 
 /*
     Uncomment the line below to remove records with null `id` values
